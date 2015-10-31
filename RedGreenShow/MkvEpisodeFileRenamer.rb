4d94e26203851f0b38f3555ee2e70cc2c2eadf3f
bryan.kahlig <bryan.kahlig@gmail.com>
2015-10-31 14:38:08 -0500

    Added better documentation and made it more generic.

--- RedGreenShow/MkvEpisodeFileRenamer.rb
+++ RedGreenShow/MkvEpisodeFileRenamer.rb
@@ -1,8 +1,25 @@
-basePath = "E:/My Movies Temp/The Red Green Show/"
-seasonPath = "Season 5/"
+# Purpose:
+# Rename files in <base path>/disc <#>/* to <base path> directory with ordered names.
+# e.g. C:\basepath\disk ##\title##.mkv files to C:\basepath\Episode 0#.mkv
+# To Use:
+# <arg1>
+# <arg1> == base path: base path should have as many
+
+basePath = ARGV[0]
 baseFilename = "Episode %02d.mkv"
 
-discPathParent = basePath + seasonPath
+
+def cleanBasePath(path)
+  fixedPath = path
+  if !fixedPath.end_with? "/"
+    fixedPath += "/"
+  end
+  return fixedPath
+end
+
+def validPath(path)
+  return Dir.exist?(path)
+end
 
 def getDirectories(path)
   return Dir.entries(path).select {|entry| File.directory? File.join(path,entry) and !(entry =='.' || entry == '..') }
@@ -23,8 +40,15 @@ def workOnDirectory(pathOfFiles, pathToPlaceFiles, baseFilename, filesRenamed)
   }
 end
 
-directories = getDirectories discPathParent
-filesRenamed = 1
+discPathParent = cleanBasePath(basePath)
+
+if !validPath(discPathParent)
+  puts 'ERROR - Invalid path: ' + discPathParent
+  exit
+end
+
+directories    = getDirectories(discPathParent)
+filesRenamed   = 1
 
 directories.each { |directory|
   puts directory
@@ -34,4 +58,3 @@ directories.each { |directory|
   filesRenamed += filesInCurrentDirectory
 }
 
-#workOnDirectory "E:/My Movies Temp/The Red Green Show/Season 3/", "E:/My Movies Temp/The Red Green Show/Season 3/", baseFilename
